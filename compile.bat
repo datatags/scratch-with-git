f7e2c9d622dab18bde0e342fb3618be5acdb6afd
datatags <datatags@users.noreply.github.com>
2018-12-25 18:36:40 -0800

    Added support for Scratch 3 and fixed stuff

--- compile.bat
+++ compile.bat
@@ -1,14 +1,27 @@
 @ECHO OFF
+set args=
 echo Preparing to compile.
 if exist project.sb2 goto confirmdel
+if exist project.sb3 goto confirmdel
 if exist project.zip goto confirmdel
 :RETURN
-echo Would you like to compile for the online or offline editor?
-echo 1) Offline editor (smaller file size, but takes an indeterminately long time to upload to the online editor.)
-echo 2) Online  editor (larger file size, works in both the online and offline editors.) [Default]
+echo Would you like to compile for the offline or online editor?
+echo 1) Smaller size (takes slightly longer, doesn't work in Scratch 2 online editor)
+echo 2) Larger size (very fast, works in Scratch 2 online editor)
 echo Please choose either 1 or 2 and press that key accordingly.
 choice /c 12
 set compression=%errorlevel%
+REM This uses the naming convention difference between Scratch 2 and 3 to detect which one we're working on:
+REM Scratch 2 projects use base-10-incrementally-named assets named as 0.*, 1.*, 2.* etc.
+REM Scratch 3 projects all use 32-character hexadecimal names.
+REM Unless the project is empty, there should be a 0.* and if it's empty why are you compiling it?!
+if exist assets/0.* (
+	echo Detected Scratch 2 project
+	set scratchver=2
+) else (
+	echo Detected Scratch 3 project
+	set scratchver=3
+)
 echo Checking for 7-Zip...
 REM for testing: goto USEPS
 set zippath=null
@@ -46,29 +59,30 @@ if %compression% equ 2 set args=-CompressionLevel NoCompression
 powershell "Compress-Archive -Path .\project.json, .\assets\*.* %args% project.zip"
 echo Changing file extension...
 REM PowerShell won't even create a zip file with an extension other than .zip so we change it afterwards.
-rename project.zip project.sb2
+rename project.zip project.sb%scratchver%
 goto END
 :USESZ
 echo Compressing files...
 if %compression% equ 2 set args=-mx=0
 REM Since 7-Zip supports compressing in several different formats, it needs to know the extension it's compressing to.
-%zippath% a %args% project.zip assets\*.* project.json
-echo Changing file extension...
-rename project.zip project.sb2
+%zippath% a %args% project.zip .\assets\*.* project.json
+echo Changing file extension as for Scratch %scratchver%...
+rename project.zip project.sb%scratchver%
 goto END
 :CONFIRMDEL
 REM All caps may not have been necessary but it gets the point across.
-echo WARNING! THIS WILL OVERWRITE project.sb2 AND project.zip!  IF YOU DO NOT WANT THEM TO BE OVERWRITTEN PLEASE RENAME THEM BEFORE CONTINUING!
-echo Do you accept that continuing will permanently and irreparably overwrite project.sb2 and project.zip?  If so, please confirm by typing yes.  Otherwise, just press enter.
+echo WARNING! THIS WILL OVERWRITE project.sb2 / project.sb3 AND project.zip!  IF YOU DO NOT WANT THEM TO BE OVERWRITTEN PLEASE RENAME THEM BEFORE CONTINUING!
+echo Do you accept that continuing will permanently and irreparably overwrite project.sb2 / project.sb3 and project.zip?  If so, please confirm by typing yes.  Otherwise, just press enter.
 REM I like little > characters when I'm supposed to type something.
 set /p confirm=^>
 if "%confirm%" equ "yes" (
 	echo Deleting.
 	if exist project.sb2 del project.sb2
+	if exist project.sb3 del project.sb3
 	if exist project.zip del project.zip
 	goto return
 ) else (
-	echo Terminating.
+	echo Script cannot continue without deleting files, terminating.
 	exit /b 1
 )
 
--- decompile.bat
+++ decompile.bat
@@ -3,26 +3,40 @@ echo Preparing for decompile.
 echo Removing old files...
 if exist assets\*.* del /Q assets\*.*
 if exist project.json del /Q project.json
+
+REM we check to see if the files exist so we don't end up doing useless stuff
+if exist *.sb2 set sb2=1
+if exist *.sb3 set sb3=1
+
+REM check if user has an ID-10T error
+if not defined sb2 (
+	if not defined sb3 (
+		echo Can't find any files to extract, exiting.
+		goto END
+	)
+)
+
 echo Checking for 7-Zip...
 REM for testing: goto USEPS
 set zippath=null
-REM Proper 7-Zip install (i.e. 64-bit on 64-bit)
+REM Proper 7-Zip install (ex. 64-bit on 64-bit)
 if exist "C:\Program Files\7-Zip\7z.exe" set zippath="C:\Program Files\7-Zip\7z.exe"
 REM Improper 7-Zip install (32-bit install on 64-bit computer)
 if exist "C:\Program Files (x86)\7-Zip\7z.exe" set zippath="C:\Program Files (x86)\7-Zip\7z.exe"
 REM Portable 7-Zip executable
 if exist "7za.exe" set zippath="7za.exe"
-if %zippath% equ null (
+if not defined zippath (
 	goto useps
 ) else (
 	goto usesz
 )
+
 REM Short for UsePowerShell. I could probably just say UPS but then system admins would think of a giant battery, and normal people would think of a package delivery service.  But this way, nobody gets confused.
 :USEPS
 echo Could not find a 7-Zip install, using PowerShell for zip extraction.
 echo You should install 7-Zip. It makes this whole process faster and easier.  That and it can help you with archives in general.
 echo Checking for correct version...
-REM So this is the best way I could find to check if the installed version of PowerShell supports archive commands.
+REM So this is the best way I could find to check if the installed version of PowerShell supports archive commands, basically run the command and detect if it fails
 for /f "tokens=* USEBACKQ" %%F in (`powershell "if (Get-Command \"Expand-Archive\" -errorAction SilentlyContinue) {echo 1}"`) do (
 	set supported=%%F
 )
@@ -37,12 +51,22 @@ if "%supported%" equ "1" (
 	pause
 	exit /b 1
 )
-echo Changing file extension...
+
+REM TODO: Extract each project in its own directory since this will get confused if there's more than one project here.
+
+echo Changing extension and extracting files...
 REM PowerShell refuses to extract anything that doesn't have a .zip extension, even if it's a zip file at heart.
-rename *.sb2 *.zip
-echo Extracting new files...
 REM Extract all files in *.zip to the folder named assets. We'll move project.json out in a minute.
-powershell "Expand-Archive -DestinationPath .\assets\ *.zip"
+if defined sb2 (
+	rename *.sb2 *.zip
+	powershell "Expand-Archive -DestinationPath .\assets\ *.zip"
+	rename *.zip *.sb2
+)
+if defined sb3 (
+	rename *.sb3 *.zip
+	powershell "Expand-Archive -DestinationPath .\assets\ *.zip"
+	rename *.zip *.sb3
+)
 echo Setting date on files...
 REM TODO: Set dates on files to same as project?
 REM So the reason this is here is that PowerShell refuses to extract dates from Scratch .sb2 files. This results in any extracted files having all interaction dates set as January 1, 1980.
@@ -53,18 +77,17 @@ REM I don't know much about PowerShell (I pieced this together from a couple Sta
 powershell "Get-ChildItem .\assets\ | Foreach-Object {$_.lastwritetime=$(Get-Date);$_.lastaccesstime=$(Get-Date);$_.creationtime=$(Get-Date)}"
 echo Moving project.json to root...
 move assets\project.json .
-echo Putting file extension back...
-rename *.zip *.sb2
 goto END
 REM Short for -- you guessed it -- Use Seven Zip or Use 7-Zip but I try to avoid using digits in labels.
 :USESZ
 echo Found a 7-Zip executable, using it.
 REM extract *.sb2 to folder assets using 7-Zip
-%zippath% x -oassets *.sb2
+if defined sb2 %zippath% x -oassets *.sb2
+if defined sb3 %zippath% x -oassets *.sb3
 echo Moving project.json to root...
 move assets\project.json .
 goto END
 :END
-echo Success.
+echo Done.
 REM Always pause at the end of a batch file because otherwise if the user double-clicked on it instead of running it from a command prompt they wouldn't see the final output becuase the window would just close.
 pause
